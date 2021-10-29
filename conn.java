3abe8bc89ef65bf62627b6e5fc3b226f47d95cd9
Hemant Arora <arorahemant75@gmail.com>
2021-10-29 20:16:38 +0530

    Create conn.java

---
+++ conn.java
@@ -0,0 +1,14 @@
+import java.sql.*;
+public class conn {
+    public Connection c;
+    public Statement s;
+    public conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+           c = DriverManager.getConnection("jdbc:mysql://localhost:3306/project11","root","");
+           s = c.createStatement();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
